441ef18b848d485069ecfcb81a655a5affae3665
Jean Carlos <jhenriquez2204@gmail.com>
2020-02-25 18:30:26 +0000

    Initial commit

---
+++ commonword.go
@@ -0,0 +1,50 @@
+package mostcommonword
+
+import "fmt"
+
+func MostCommonWord(paragraph string, banned []string) string {
+  wordEndings := map[byte]bool {
+    '.': true,
+    ',': true,
+    ';': true,
+    '!': true,
+    ' ': true,
+    '\t': true,
+    '\n': true,
+  }
+
+  bannedLkup := make(map[string] bool, len(banned))
+
+  for _, ban := range banned {
+    bannedLkup[ban] = true
+  }
+
+  var maxWord string
+  var maxWordCount int
+  wordsCount := make(map[string]int, 10)
+  var currWord string
+
+  for i := range paragraph {
+    letter := paragraph[i]
+
+    if _, wordEnded := wordEndings[letter]; wordEnded {
+
+      if _, baned := bannedLkup[currWord]; len(currWord) > 0 && !baned {
+        wordCount := wordsCount[currWord]
+        wordsCount[currWord] = wordCount+1
+
+        if wordCount+1 > maxWordCount {
+          maxWordCount = wordCount+1
+          maxWord = currWord
+        }
+
+      } 
+
+      currWord = ""
+    } else {
+     currWord = fmt.Sprintf("%s%c", currWord, letter)
+    }
+  }
+
+  return maxWord
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jchenriquez/2973
+
+go 1.13
