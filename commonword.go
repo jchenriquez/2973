7dbeb18310c03a8e5202df4c2dcd384de70d73fa
jchenriquez <jc.henriquez04@gmail.com>
2020-02-25 20:52:22 -0500

    solution

--- commonword.go
+++ commonword.go
@@ -1,51 +1,59 @@
 package mostcommonword
 
-import ("fmt"
-"unicode"
+import (
+	"fmt"
+	"unicode"
 )
 
+// MostCommonWord will return the most non-banned word in parahraph
 func MostCommonWord(paragraph string, banned []string) string {
-  wordEndings := map[byte]bool {
-    '.': true,
-    ',': true,
-    ';': true,
-    '!': true,
-    ' ': true,
-    '\t': true,
-    '\n': true,
-  }
+	wordEndings := map[byte]bool{
+		'.':  true,
+		',':  true,
+		';':  true,
+		'!':  true,
+		' ':  true,
+		'\t': true,
+		'\n': true,
+		'?':  true,
+		'\'': true,
+	}
 
-  bannedLkup := make(map[string] bool, len(banned))
+	bannedLkup := make(map[string]bool, len(banned))
 
-  for _, ban := range banned {
-    bannedLkup[ban] = true
-  }
+	for _, ban := range banned {
+		bannedLkup[ban] = true
+	}
 
-  var maxWord string
-  var maxWordCount int
-  wordsCount := make(map[string]int, 10)
-  var currWord string
+	var maxWord string
+	var maxWordCount int
+	wordsCount := make(map[string]int, 10)
+	var currWord string
 
-  for i := range paragraph {
-    letter := paragraph[i]
+	for i := 0; i <= len(paragraph); i++ {
+		var letter byte
 
-    if _, wordEnded := wordEndings[letter]; wordEnded {
-      if _, baned := bannedLkup[currWord]; len(currWord) > 0 && !baned {
-        wordCount := wordsCount[currWord]
-        wordsCount[currWord] = wordCount+1
+		if i < len(paragraph) {
+			letter = paragraph[i]
+		}
 
-        if wordCount+1 > maxWordCount {
-          maxWordCount = wordCount+1
-          maxWord = currWord
-        }
+		if _, wordEnded := wordEndings[letter]; wordEnded || i == len(paragraph) {
+			if _, baned := bannedLkup[currWord]; len(currWord) > 0 && !baned {
+				wordCount := wordsCount[currWord]
+				wordsCount[currWord] = wordCount + 1
 
-      } 
+				if wordCount+1 > maxWordCount {
+					maxWordCount = wordCount + 1
+					maxWord = currWord
+				}
 
-      currWord = ""
-    } else {
-     currWord = fmt.Sprintf("%s%c", currWord, unicode.Lower(letter))
-    }
-  }
+			}
 
-  return maxWord
+			currWord = ""
+		} else {
+			currWord = fmt.Sprintf("%s%c", currWord, unicode.ToLower(rune(letter)))
+		}
+	}
+
+	return maxWord
 }
